01e12f8a0dc6e5e353bd96a2236c810d4443179e
Longas13 <g.pacevicius@gmail.com>
2021-09-29 23:50:43 +0300

    add global styles. Add About Section style

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,8 +1,10 @@
 import AboutUs from "./pages/AboutUs";
+import GlobalStyles from "./components/GlobalStyle";
 
 function App () {
   return (
     <div className="App">
+      <GlobalStyles />
       <AboutUs />
     </div>
   );
