2539e1ddd024bd3ed69d11e3bb22b9cde9e5ae8f
Longas13 <g.pacevicius@gmail.com>
2021-10-14 23:16:06 +0300

    add Nav + styles

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,10 +1,12 @@
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 import GlobalStyles from "./components/GlobalStyle";
 
 function App () {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <AboutUs />
     </div>
   );
